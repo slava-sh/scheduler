7e01a82815057ad42e9581bc4fe7aa55e70c8dea
Slava Shklyaev <slava@slava.sh>
2016-04-29 15:45:33 +0300

    Plot more

--- plots.m
+++ plots.m
@@ -5,6 +5,7 @@ function plots(test)
   invokerTime = data(:, 3);
   invocations = data(:, 4);
   testCount   = data(:, 5);
+  timeLimit   = data(:, 6);
 
   subplot(2, 2, 1);
   scatter(submitTime, invokerTime, 8, problem, 'x');
@@ -17,6 +18,11 @@ function plots(test)
   ylabel('invocations');
 
   subplot(2, 2, 3);
+  scatter(submitTime, timeLimit .* testCount, 8, problem, 'x');
+  xlabel('submit time');
+  ylabel('time limit * test count');
+
+  subplot(2, 2, 4);
   scatter(submitTime, testCount, 8, problem, 'x');
   xlabel('submit time');
   ylabel('test count');

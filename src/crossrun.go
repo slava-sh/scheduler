35e1ba3a88c125399dab97528544944507760baa
Slava Shklyaev <slava@slava.sh>
2016-05-02 20:33:14 +0300

    Improve error handling in runner

--- src/crossrun.go
+++ src/crossrun.go
@@ -4,29 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"sync"
 )
 
-func run(test int) int {
+func run(test int) (score int, err error) {
 	testFile := fmt.Sprintf("tests/%02d", test)
 	outputFile, err := ioutil.TempFile("", "output")
 	if err != nil {
-		log.Panic(err)
+		return
 	}
 	defer os.Remove(outputFile.Name())
 	interactor := exec.Command("bin/interactor", testFile, outputFile.Name())
 	scheduler := exec.Command("bin/scheduler")
 	interactor.Stdin, err = scheduler.StdoutPipe()
 	if err != nil {
-		log.Panic(err)
+		return
 	}
 	scheduler.Stdin, err = interactor.StdoutPipe()
 	if err != nil {
-		log.Panic(err)
+		return
 	}
 	interactor.Stderr = os.Stderr
 	scheduler.Stderr = os.Stderr
@@ -34,21 +33,17 @@ func run(test int) int {
 	scheduler.Start()
 	err = interactor.Wait()
 	if err != nil {
-		log.Panic(err)
+		return
 	}
 	err = scheduler.Wait()
 	if err != nil {
-		log.Panic(err)
+		return
 	}
 	output, err := bufio.NewReader(outputFile).ReadString('\n')
 	if err != nil {
-		log.Panic(err)
+		return
 	}
-	score, err := strconv.Atoi(output[:len(output)-1])
-	if err != nil {
-		log.Panic(err)
-	}
-	return score
+	return strconv.Atoi(output[:len(output)-1])
 }
 
 func main() {
@@ -57,7 +52,7 @@ func main() {
 		for _, word := range os.Args[1:] {
 			test, err := strconv.Atoi(word)
 			if err != nil {
-				log.Panic(err)
+				panic(err)
 			}
 			tests = append(tests, test)
 		}
@@ -73,7 +68,11 @@ func main() {
 		wg.Add(1)
 		go func(test int) {
 			defer wg.Done()
-			score := run(test)
+			score, err := run(test)
+			if err != nil {
+				fmt.Printf("%v\t%v\n", test, err)
+				return
+			}
 			scores[test] = score
 			fmt.Printf("%v\t%v\n", test, score)
 		}(test)
